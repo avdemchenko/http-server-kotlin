a30d436a910744c0d4c06c7c535e6415b1a7e9e8
Oleksii Demchenko <oleksii.demchenko@leovegas.com>
2024-07-22 13:15:22 +0300

    Extract URL path

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -5,14 +5,28 @@ import java.net.ServerSocket
 fun main() {
     val serverSocket = ServerSocket(4221)
     serverSocket.reuseAddress = true
-    serverSocket.reuseAddress = true
 
     val socket = serverSocket.accept()
     println("New connection has been accepted")
 
     socket.use {
         val writer = socket.getOutputStream().bufferedWriter()
-        writer.write("HTTP/1.1 200 OK\r\n\r\n")
+        val reader = socket.getInputStream().bufferedReader()
+        val request = reader.readLine()
+        val headers = mutableListOf<String>()
+        var read = reader.readLine()
+        while (read != null && read.isNotEmpty()) {
+            headers.add(read)
+            read = reader.readLine()
+        }
+        println("request: $request, headers: $headers")
+        val requestArgs = request.split(Regex("\\s+"))
+        val requestTarget = requestArgs[1]
+        if (requestTarget == "/") {
+            writer.write("HTTP/1.1 200 OK\r\n\r\n")
+        } else {
+            writer.write("HTTP/1.1 404 Not Found\r\n\r\n")
+        }
         writer.flush()
     }
 }
