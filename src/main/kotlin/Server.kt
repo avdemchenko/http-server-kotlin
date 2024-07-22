f2390e5ffb9dd5c4c922e78572667002fe7b9487
Oleksii Demchenko <oleksii.demchenko@leovegas.com>
2024-07-22 14:41:54 +0300

    Respond with body

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -19,11 +19,21 @@ fun main() {
             headers.add(read)
             read = reader.readLine()
         }
-        println("request: $request, headers: $headers")
+        println("request: $request")
+        println("headers: $headers")
+
         val requestArgs = request.split(Regex("\\s+"))
         val requestTarget = requestArgs[1]
         if (requestTarget == "/") {
             writer.write("HTTP/1.1 200 OK\r\n\r\n")
+        } else if (requestTarget.startsWith("/echo/")) {
+            val str = requestTarget.substringAfter("/echo/")
+            writer.write(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${
+                    str.encodeToByteArray()
+                        .size
+                }\r\n\r\n$str"
+            )
         } else {
             writer.write("HTTP/1.1 404 Not Found\r\n\r\n")
         }
